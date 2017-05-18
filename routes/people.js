37264fc25ee1f7edebfda7d9c5cf778b4050d052
Saulo Antunes <sauloantuness@gmail.com>
2017-05-18 13:44:53 -0300

    exercicio

--- routes/people.js
+++ routes/people.js
@@ -65,7 +65,20 @@ router.get('/new/', function(req, res) {
 //   2. Redirecionar para a rota de listagem de pessoas
 //      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
 //      - Em caso de erro do INSERT, colocar mensagem vermelhinha
+router.post('/', function(req, res) {
+  var query = 'INSERT INTO person (name) values (' + db.escape(req.body.name) + ')';
+  
+  db.query(query, function(err, result) {
+    if (err) {
+      req.flash('error', 'Deu ruim! - ' + err)
+    }
+    else {
+      req.flash('success', 'A pessoa foi inserida!')
+    }
 
+    res.redirect('/people')    
+  });
+});
 
 /* DELETE uma pessoa */
 // Exercício 2: IMPLEMENTAR AQUI
@@ -74,5 +87,19 @@ router.get('/new/', function(req, res) {
 //   2. Redirecionar para a rota de listagem de pessoas
 //      - Em caso de sucesso do INSERT, colocar uma mensagem feliz
 //      - Em caso de erro do INSERT, colocar mensagem vermelhinha
+router.delete('/:id', function(req, res) {
+  var query = 'DELETE FROM person where id = ' + req.params.id;
+  
+  db.query(query, function(err, result) {
+    if (err) {
+      req.flash('error', 'Deu ruim! - ' + err)
+    }
+    else {
+      req.flash('success', 'A pessoa foi deletada!')
+    }
+
+    res.redirect('/people')    
+  });
+});
 
 module.exports = router;
